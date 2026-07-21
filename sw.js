6513fcb32eaaa6ecdf8edc0c972693aa612b131a
Fergus Knies <70333234+Kawsfan@users.noreply.github.com>
2026-07-21 20:29:12 +0200

    bump sw cache to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "autovergelijker-v6";
+const CACHE = "autovergelijker-v7";
 const STATIC = ["/autovergelijker/","/autovergelijker/index.html"];
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(STATIC);}));
